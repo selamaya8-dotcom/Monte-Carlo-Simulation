7300e368d11a4caeedd4b6088c1c7f71b70fd6d3
selamaya8-dotcom <selamaya8@gmail.com>
2026-03-04 18:11:17 +0200

    Update MuonDetectorB.hh

--- include/MuonDetectorB.hh
+++ include/MuonDetectorB.hh
@@ -1,18 +1,29 @@
-#ifndef MUONDETECTORB_HH
-#define MUONDETECTORB_HH
+#ifndef MUONDETECTORA_HH
+#define MUONDETECTORA_HH
 
 #include "G4VSensitiveDetector.hh"
 #include "MuonHit.hh"
 
-class MuonDetectorB : public G4VSensitiveDetector {
+class G4Step;
+class G4HCofThisEvent;
+class G4TouchableHistory;
+
+/**
+ * @brief Sensitive Detector for volume A.
+ * Tracks muon energy deposition and interaction timing.
+ */
+class MuonDetectorA : public G4VSensitiveDetector {
 public:
-    MuonDetectorB(G4String name);
-    virtual ~MuonDetectorB() = default;
-    void Initialize(G4HCofThisEvent* hce) override;
-    G4bool ProcessHits(G4Step*, G4TouchableHistory*) override;
+    MuonDetectorA(G4String name);
+    virtual ~MuonDetectorA() override = default;
+
+    // Standard Geant4 SD interface
+    virtual void   Initialize(G4HCofThisEvent* hce) override;
+    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history) override;
+
 private:
-    MuonHitsCollection* fHitsCollection = nullptr;
-    G4int fHCID = -1;
+    MuonHitsCollection* fHitsCollection = nullptr; // Managed by G4SDManager
+    G4int fHCID = -1;                              // Hit Collection ID
 };
 
 #endif
