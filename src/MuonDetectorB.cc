ab0c8783833a5a02150d7bc360b4d3d31829a228
selamaya8-dotcom <selamaya8@gmail.com>
2026-03-04 17:58:23 +0200

    Update MuonDetectorB.cc

--- src/MuonDetectorB.cc
+++ src/MuonDetectorB.cc
@@ -2,30 +2,38 @@
 #include "G4Step.hh"
 #include "G4SDManager.hh"
 #include "G4HCofThisEvent.hh"
-#include "G4TouchableHistory.hh"
 
 MuonDetectorB::MuonDetectorB(G4String name)
-: G4VSensitiveDetector(name), fHitsCollection(nullptr), fHCID(-1) {
+: G4VSensitiveDetector(name), fHitsCollection(nullptr), fHCID(-1) 
+{
+    // Define the specific hit collection name for Detector B
     collectionName.insert("muonHitCollectionB");
 }
 
-void MuonDetectorB::Initialize(G4HCofThisEvent* hce) {
+void MuonDetectorB::Initialize(G4HCofThisEvent* hce) 
+{
     fHitsCollection = new MuonHitsCollection(SensitiveDetectorName, collectionName[0]);
+    
     if (fHCID < 0) {
-        fHCID = G4SDManager::GetSDMpointer()->GetCollectionID(fHitsCollection);
+        fHCID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
     }
+    
     hce->AddHitsCollection(fHCID, fHitsCollection);
 }
 
-G4bool MuonDetectorB::ProcessHits(G4Step* step, G4TouchableHistory*) {
-    if (!fHitsCollection) return false;
-    auto edep = step->GetTotalEnergyDeposit();
+G4bool MuonDetectorB::ProcessHits(G4Step* step, G4TouchableHistory*) 
+{
+    G4double edep = step->GetTotalEnergyDeposit();
     if (edep <= 0.) return false;
-    auto preStepPoint = step->GetPreStepPoint();
+
+    G4StepPoint* preStepPoint = step->GetPreStepPoint();
+    
     MuonHit* hit = new MuonHit();
     hit->SetEdep(edep);
     hit->SetPos(preStepPoint->GetPosition());
     hit->SetTime(preStepPoint->GetGlobalTime());
+
     fHitsCollection->insert(hit);
+    
     return true;
 }
