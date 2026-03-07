dd54a1ed95116fe3c1e04564786be6ba235e487a
selamaya8-dotcom <selamaya8@gmail.com>
2026-03-07 19:46:25 +0200

    Update EventAction.hh

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -4,26 +4,27 @@
 #include "G4UserEventAction.hh"
 #include "globals.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+/**
+ * @brief Handles data collection at the end of each event.
+ * Consolidates hit information from multiple sensitive detectors and 
+ * logs data to both the console and a CSV file.
+ */
 class EventAction : public G4UserEventAction
 {
   public:
     EventAction();
-   ~EventAction();
+    virtual ~EventAction() override;
 
-   void SetGenAngle(G4double val) { fGenAngle = val; }
-   void SetGenEnergy(G4double val) { fGenEnergy = val; }
+    virtual void BeginOfEventAction(const G4Event* event) override;
+    virtual void EndOfEventAction(const G4Event* event) override;
 
-    virtual void BeginOfEventAction(const G4Event*);
-    virtual void   EndOfEventAction(const G4Event*);
+    // Setters for generator metadata
+    void SetGenAngle(G4double val)  { fGenAngle = val; }
+    void SetGenEnergy(G4double val) { fGenEnergy = val; }
+
   private:
     G4double fGenAngle;
     G4double fGenEnergy;
-
-
 };
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
