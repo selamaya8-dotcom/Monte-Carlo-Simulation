7421d9151a09af39cc10be29d1752724ba2c0387
selamaya8-dotcom <selamaya8@gmail.com>
2026-03-21 23:42:34 +0200

    Refactor DetectorConstruction class and update members

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -4,53 +4,49 @@
 #include "G4VUserDetectorConstruction.hh"
 #include "globals.hh"
 #include "G4ThreeVector.hh"
-#include "G4GenericMessenger.hh"
 #include "G4SystemOfUnits.hh"
 
-
-
 class G4LogicalVolume;
+class G4VPhysicalVolume;
+class G4GenericMessenger;
 
+/**
+ * @brief Constructs the world, a soil target (cube or sphere), and a 
+ * plastic scintillator detector encased in a cardboard box.
+ */
 class DetectorConstruction : public G4VUserDetectorConstruction {
 public:
     DetectorConstruction();
-    ~DetectorConstruction();
-
+    virtual ~DetectorConstruction();
 
     virtual G4VPhysicalVolume* Construct() override;
-
     virtual void ConstructSDandField() override;
 
-
+    // Accessors for PrimaryGeneratorAction to ensure muons are aimed correctly
     const G4ThreeVector& GetDetCenter() const { return fDetCenter; }
-    G4ThreeVector GetDetHalfSize() const { return {fDetHx, fDetHy, fDetHz}; }
-    G4double GetWorldXHalfLength() const { return fWorldHx; }
     G4ThreeVector GetBuildingHalfSize() const { return {fBuildHx, fBuildHy, fBuildHz}; }
-    const G4ThreeVector& GetBuildingCenter() const { return fBuildCenter; }
-
+    
+    // Setters
     void SetSoilDensity(G4double density) { fSoilDensity = density; }
 
 private:
-    G4double fSoilDensity = 0.2*g/cm3;
-    G4GenericMessenger* fMessenger;
-    G4LogicalVolume* fDetectorLogic = nullptr;
-    G4LogicalVolume* det1LV = nullptr;
-    G4LogicalVolume* det2LV = nullptr;
-
-
     void DefineMaterials();
     G4VPhysicalVolume* ConstructVolumes();
 
-    G4double fDetHx=0, fDetHy=0, fDetHz=0;
-    G4ThreeVector fDetCenter = {0,0,0};
-    G4double fBuildHx=0, fBuildHy=0, fBuildHz=0;
-    G4ThreeVector fBuildCenter = {0,0,0};
+    // Messenger for UI commands
+    G4GenericMessenger* fMessenger;
 
-    G4VPhysicalVolume* fBuildingPV = nullptr;
+    // Logical Volumes for SD assignment
+    G4LogicalVolume* det1LV = nullptr;
+    G4LogicalVolume* det2LV = nullptr;
 
-    G4double fWorldHx = 0;
-    G4double fSkyZ = 0;
+    // Geometry parameters
+    G4double fSoilDensity;
+    G4double fDetHx, fDetHy, fDetHz;
+    G4ThreeVector fDetCenter;
+    
+    G4double fBuildHx, fBuildHy, fBuildHz;
+    G4ThreeVector fBuildCenter;
 };
-
 
 #endif
