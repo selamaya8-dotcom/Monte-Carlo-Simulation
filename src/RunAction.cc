f26f2f906ab784d6faff7d981c84fe04b6e06896
selamaya8-dotcom <selamaya8@gmail.com>
2026-05-01 18:03:16 +0300

    Clean up RunAction by removing unused code
    Removed commented-out code and unused includes.

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,22 +1,14 @@
-// RunAction.cc
 #include "RunAction.hh"
-//#include "HistoManager.hh"
 
 #include "G4Run.hh"
 #include "G4AnalysisManager.hh"
 
 RunAction::RunAction()
-  : G4UserRunAction()//, fHistoManager(0)
+  : G4UserRunAction()
   {
-
-  //fHistoManager = new HistoManager();
-
-
-
   }
 
 RunAction::~RunAction() {
-  //delete fHistoManager;
 }
 
 void RunAction::BeginOfRunAction(const G4Run*)
@@ -26,17 +18,10 @@ void RunAction::BeginOfRunAction(const G4Run*)
   std::ofstream outfile("hits_output.csv");
   outfile << "Detector,EventID,PosX,PosY,PosZ,MomX,MomY,MomZ,Edep,GenAngle,GenEnergy,GenPosX,GenPosY,GenPosZ\n";
   outfile.close();
-/*
-  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-  analysisManager->OpenFile();
-*/
+
 }
 
 void RunAction::EndOfRunAction(const G4Run*)
 {
-  /*
-  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-  analysisManager->Write();
-  analysisManager->CloseFile();
-*/
+
 }
