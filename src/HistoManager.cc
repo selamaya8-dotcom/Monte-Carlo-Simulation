6abd55ab1e2e6c92fd76cd56ac6ed0f0fa252a6f
selamaya8-dotcom <selamaya8@gmail.com>
2026-05-01 17:59:41 +0300

    Clean up comments in HistoManager.cc
    Removed unnecessary comment lines in HistoManager.

--- src/HistoManager.cc
+++ src/HistoManager.cc
@@ -4,21 +4,16 @@
 #include "G4Event.hh"
 #include "G4RunManager.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 HistoManager::HistoManager()
   : fFileName("DroneMuonScan")
 {
   Book();
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 HistoManager::~HistoManager()
 {
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 void HistoManager::Book()
 {
   G4AnalysisManager* analysis = G4AnalysisManager::Instance();
