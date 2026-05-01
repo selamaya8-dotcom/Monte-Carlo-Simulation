6ce1b22be5b0ad158ef7ad9c3ece13abf898e012
selamaya8-dotcom <selamaya8@gmail.com>
2026-05-01 17:57:18 +0300

    Clean up comments in ActionInitialization
    Removed comments from BuildForMaster and Build methods.

--- src/ActionInitialization.cc
+++ src/ActionInitialization.cc
@@ -13,21 +13,17 @@ ActionInitialization::~ActionInitialization()
 
 void ActionInitialization::BuildForMaster() const
 {
-  // Master thread only handles RunAction (for merging global results)
   RunAction* runAction = new RunAction();
   SetUserAction(runAction);
 }
 
 void ActionInitialization::Build() const {
-    // Primary Generator: Sources particles for each thread
     PrimaryGeneratorAction* primary = new PrimaryGeneratorAction(fDetector);
     SetUserAction(primary);
 
-    // RunAction: Handles start/end of simulation runs
     RunAction* runAction = new RunAction();
     SetUserAction(runAction);
 
-    // EventAction: Handles data collection at the end of each event
     EventAction* event = new EventAction();
     SetUserAction(event);
 
