9b38a158a0fa1de7794b35b1162e1bffcbce0ad9
selamaya8-dotcom <selamaya8@gmail.com>
2026-05-01 17:55:27 +0300

    Remove comment block from EventAction.hh
    Removed detailed comment block describing the EventAction class functionality.

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -5,11 +5,7 @@
 #include "globals.hh"
 #include "G4ThreeVector.hh"
 
-/**
- * @brief Handles data collection at the end of each event.
- * Consolidates hit information from multiple sensitive detectors and
- * logs data to both the console and a CSV file.
- */
+
 class EventAction : public G4UserEventAction
 {
   public:
@@ -19,7 +15,6 @@ class EventAction : public G4UserEventAction
     virtual void BeginOfEventAction(const G4Event* event) override;
     virtual void EndOfEventAction(const G4Event* event) override;
 
-    // Setters for generator metadata
     void SetGenAngle(G4double val)  { fGenAngle = val; }
     void SetGenEnergy(G4double val) { fGenEnergy = val; }
     void SetGenPosition(G4ThreeVector pos) { fGenPosition = pos; }
