f6f1e3167cb02e4492f67c00dff3d055e3d2f63c
selamaya8-dotcom <selamaya8@gmail.com>
2026-05-04 22:26:48 +0300

    Comment out G4Random include and clean up code

--- DroneMuonScan.cc
+++ DroneMuonScan.cc
@@ -1,7 +1,6 @@
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #include "G4Types.hh"
-//#include "G4Random.hh" also for seeding
+//#include "G4Random.hh" 
 #include <ctime>
 
 #include "G4RunManagerFactory.hh"
@@ -18,39 +17,30 @@
 #include "G4UIExecutive.hh"
 #include "G4VisExecutive.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 int main(int argc,char** argv) {
 
-  //detect interactive mode (if no arguments) and define UI session
   G4UIExecutive* ui = 0;
   if (argc == 1) ui = new G4UIExecutive(argc,argv);
 
   //choose the Random engine
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  // 1. Get the current system time
   long seeds[2];
   time_t systime = time(NULL);
 
-  // 2. Set seeds based on time
   seeds[0] = (long) systime;
 
   seeds[1] = (long) (systime * G4UniformRand());
 
-  // 3. Apply to the engine
   G4Random::setTheSeeds(seeds);
 
-
-  //use G4SteppingVerboseWithUnits
   G4int precision = 4;
   G4SteppingVerbose::UseBestUnit(precision);
 
-  //construct the run manager
   auto runManager = G4RunManagerFactory::CreateRunManager();
   runManager->SetNumberOfThreads(1);
 
-  //set mandatory initialization classes
   DetectorConstruction* det= new DetectorConstruction;
   runManager->SetUserInitialization(det);
 
@@ -59,10 +49,8 @@ int main(int argc,char** argv) {
 
   runManager->SetUserInitialization(new ActionInitialization(det));
 
-  //initialize visualization
   G4VisManager* visManager = nullptr;
 
-  // get the pointer to the User Interface manager
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
 
@@ -81,7 +69,6 @@ int main(int argc,char** argv) {
    UImanager->ApplyCommand(command+fileName);
   }
 
-  //job termination
   delete visManager;
   delete runManager;
 }
